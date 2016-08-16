295acbe73709739de8a7294c609503bac26771d7
표병식 <byungsik.pyo@hyundai-uni.com>
2016-08-16 09:05:20 +0900

    사용자가 time out 시간 설정 가능하도록 수정

--- src/main/java/com/hyundaiuni/nxtims/framework/api/RestApiTemplate.java
+++ src/main/java/com/hyundaiuni/nxtims/framework/api/RestApiTemplate.java
@@ -6,26 +6,34 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestApiTemplate {
-    private RestTemplate template;
-    private int timeout = 3 * 60 * 1000;
+    private int DEFAULT_TIMEOUT = 3 * 60 * 1000;
 
-    public RestApiTemplate() {
-        this.template = new RestTemplate(getClientHttpRequestFactory());
-    }
+    private RequestConfig config;
 
     public RestTemplate getRestTemplate() {
-        return template;
+        config = RequestConfig.custom().setConnectTimeout(DEFAULT_TIMEOUT).setConnectionRequestTimeout(
+            DEFAULT_TIMEOUT).setSocketTimeout(DEFAULT_TIMEOUT).build();
+
+        return new RestTemplate(getClientHttpRequestFactory());
+    }
+
+    public RestTemplate getRestTemplate(int timeout) {
+        Assert.isTrue(timeout >= 0, "Timeout must be a non-negative value");
+
+        config = RequestConfig.custom().setConnectTimeout(timeout).setConnectionRequestTimeout(
+            timeout).setSocketTimeout(timeout).build();
+
+        return new RestTemplate(getClientHttpRequestFactory());
     }
 
     private ClientHttpRequestFactory getClientHttpRequestFactory() {
-        RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout).setConnectionRequestTimeout(
-            timeout).setSocketTimeout(timeout).build();
-        CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
 
-        return new HttpComponentsClientHttpRequestFactory(client);
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
