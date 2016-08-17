1ca740596d95911da32351dfdc673b526fab780f
표병식 <byungsik.pyo@hyundai-uni.com>
2016-08-17 17:30:48 +0900

    replace

--- src/main/java/com/hyundaiuni/nxtims/framework/security/SecurityConfiguration.java
+++ src/main/java/com/hyundaiuni/nxtims/framework/security/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,11 +16,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers("/index.html").permitAll().anyRequest().authenticated();
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
