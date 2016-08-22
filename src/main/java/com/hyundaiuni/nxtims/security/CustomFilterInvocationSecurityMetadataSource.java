22cb5a496da2b3ad8933a23813c8edf63b6ed1fc
표병식 <byungsik.pyo@hyundai-uni.com>
2016-08-22 18:55:59 +0900

    권한관련 프로그램 개발

---
+++ src/main/java/com/hyundaiuni/nxtims/security/CustomFilterInvocationSecurityMetadataSource.java
@@ -0,0 +1,88 @@
+package com.hyundaiuni.nxtims.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.hyundaiuni.nxtims.service.app.SecuredObjectService;
+
+public class CustomFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+    private static final Log log = LogFactory.getLog(CustomFilterInvocationSecurityMetadataSource.class);
+
+    private SecuredObjectService securedObjectService;
+
+    public void setSecuredObjectService(SecuredObjectService securedObjectService) {
+        this.securedObjectService = securedObjectService;
+    }
+
+    private final Map<RequestMatcher, Collection<ConfigAttribute>> requestMap;
+
+    public CustomFilterInvocationSecurityMetadataSource(
+        LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>> requestMap) {
+        this.requestMap = requestMap;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        HttpServletRequest request = ((FilterInvocation)object).getRequest();
+
+        Collection<ConfigAttribute> result = null;
+
+        for(Map.Entry<RequestMatcher, Collection<ConfigAttribute>> entry : requestMap.entrySet()) {
+            if(entry.getKey().matches(request)) {
+                result = entry.getValue();
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        Set<ConfigAttribute> allAttributes = new HashSet<ConfigAttribute>();
+
+        for(Map.Entry<RequestMatcher, Collection<ConfigAttribute>> entry : requestMap.entrySet()) {
+            allAttributes.addAll(entry.getValue());
+        }
+
+        return allAttributes;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+
+    public void reload() throws Exception {
+        LinkedHashMap<RequestMatcher, List<ConfigAttribute>> reloadedMap = securedObjectService.getRolesAndUrl();
+
+        Iterator<Entry<RequestMatcher, List<ConfigAttribute>>> iterator = reloadedMap.entrySet().iterator();
+
+        requestMap.clear();
+
+        while(iterator.hasNext()) {
+            Entry<RequestMatcher, List<ConfigAttribute>> entry = iterator.next();
+
+            requestMap.put(entry.getKey(), entry.getValue());
+        }
+
+        if(log.isInfoEnabled()) {
+            log.info("Secured Url Resources - Role Mappings reloaded at Runtime!");
+        }
+    }
+}
