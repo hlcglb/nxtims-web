4544736f85f4ddd4e406a15395b38ff391a90f53
표병식 <byungsik.pyo@hyundai-uni.com>
2016-08-29 17:03:16 +0900

    .

--- src/main/java/com/hyundaiuni/nxtims/controller/app/AppRedirectController.java
+++ src/main/java/com/hyundaiuni/nxtims/controller/app/AppRedirectController.java
@@ -1,16 +1,12 @@
 package com.hyundaiuni.nxtims.controller.app;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class AppRedirectController {
-    @Autowired
-    
     @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
-        // Forward to home page so that route is preserved.
         return "forward:/";
     }
 }
