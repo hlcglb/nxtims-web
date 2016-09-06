92c5f70e9adc1d6ce4a27df3bffc77be1c88ca6a
표병식 <byungsik.pyo@hyundai-uni.com>
2016-09-06 09:05:11 +0900

    .

--- src/test/java/com/hyundaiuni/nxtims/ApplicationTests.java
+++ src/test/java/com/hyundaiuni/nxtims/ApplicationTests.java
@@ -1,61 +1,13 @@
 package com.hyundaiuni.nxtims;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-    private static final Log log = LogFactory.getLog(ApplicationTests.class);
-
-    @Autowired
-    private WebApplicationContext context;
-    private MockMvc mvc;
-
-    @Before
-    public void setUp() {
-        Exception ex = null;
-
-        try {
-            this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-        }
-        catch(Exception e) {
-            log.error(e.getMessage());
-            ex = e;
-        }
-
-        assertEquals(null, ex);
-    }
-
     @Test
     public void contextLoads() {}
-
-    @Test
-    public void testGet() {
-        Exception ex = null;
-
-        try {
-            mvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
-        }
-        catch(Exception e) {
-            log.error(e.getMessage());
-            ex = e;
-        }
-
-        assertEquals(null, ex);
-    }
 }
