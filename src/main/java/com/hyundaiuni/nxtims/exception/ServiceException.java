824b475bab8bd17cfd5f781d7059c726d3894b14
표병식 <byungsik.pyo@hyundai-uni.com>
2016-09-09 08:41:22 +0900

    소스 수정 by sonarqube

--- src/main/java/com/hyundaiuni/nxtims/exception/ServiceException.java
+++ src/main/java/com/hyundaiuni/nxtims/exception/ServiceException.java
@@ -3,7 +3,7 @@ package com.hyundaiuni.nxtims.exception;
 public class ServiceException extends RuntimeException {
     private static final long serialVersionUID = -9061697529915101565L;
 
-    private String code = null;
+    private final String code;
 
     public ServiceException(String code, String message) {
         this(code, message, null);
